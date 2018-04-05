539ab8c6d97d2e89dd1752f472828bbc89a39431
Joro Georgiev <georgievj@georgievj-m01.vmware.com>
2018-04-05 20:23:47 +0300

    Initial commit

---
+++ src/Arrays.java
@@ -0,0 +1,26 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        foreachExample();
+    }
+        private static void foreachExample() {
+            int arr[] = new int[]{1, 2, 3, 4, 5};
+            for (int element : arr) {
+                System.out.println(element);
+            }
+        }
+    public  static void array1(String[] args) {
+        int arr[][] = new int[3][];
+        arr[0] = new int[] {1};
+        arr[1] = new int[] {1,2};
+        arr[2] = new int[] {1,2,3};
+        arr[3] = new int[] {4};
+
+        for (int i; i<10; i++) {
+            for (int j; j<)
+        }
+
+}
+}
+
+
